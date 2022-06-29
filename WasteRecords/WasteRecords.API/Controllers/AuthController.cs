8358c3ca28dfe2bb32aaa81f75245a31f0f3969c
Kadir Turan <kadirrturan@gmail.com>
2022-06-29 21:20:22 +0300

    Add SignIn endpoint

--- WasteRecords/WasteRecords.API/Controllers/AuthController.cs
+++ WasteRecords/WasteRecords.API/Controllers/AuthController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WasteRecords.API.CustomFilters;
+using WasteRecords.Core.Dtos.User;
+using WasteRecords.Core.Entities;
+using WasteRecords.Core.Helpers;
+using WasteRecords.Core.Interfaces.Services;
+using WasteRecords.Service.Tools.JWT;
 
 namespace WasteRecords.API.Controllers
 {
@@ -7,5 +14,40 @@ namespace WasteRecords.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IUserService _userService;
+        private readonly IJwtService _jwtService;
+        private readonly IMapper _mapper;
+
+        public AuthController(IUserService userService, IJwtService jwtService, IMapper mapper)
+        {
+            _userService = userService;
+            _jwtService = jwtService;
+            _mapper = mapper;
+        }
+        [HttpPost("[action]")]
+        [ValidModel]
+        public async Task<IActionResult> SignIn(UserSignInDto userSignInDto)
+        {
+            Response<string> response = new Response<string>();
+            var user = await _userService.CheckUserAsync(_mapper.Map<User>(userSignInDto));
+            if(user != null)
+            {
+                response.Message = "Login is success";
+                response.StatusCode = 201;
+                response.IsSuccess = true;
+                response.Content = _jwtService.GenerateJwt(user).Token;
+                return Created("", response);
+            }
+            else
+            {
+                response.Message = "User email or password is wrong";
+                response.StatusCode = 400;
+                response.IsSuccess = false;
+                response.Errors = new Dictionary<string, List<string>>(){
+                    {"BadRequest", new List<string>(){ "User email or password is wrong" }} 
+                };
+                return BadRequest(response);
+            }
+        }
     }
 }
--- WasteRecords/WasteRecords.Core/Helpers/Response.cs
+++ WasteRecords/WasteRecords.Core/Helpers/Response.cs
@@ -1,29 +1,18 @@
-﻿using Newtonsoft.Json;
-
-namespace WasteRecords.Core.Helpers
+﻿namespace WasteRecords.Core.Helpers
 {
     public class Response
     {
-        public string StatusCode { get; set; }
+        public int StatusCode { get; set; }
         public string Message { get; set; }
         public bool IsSuccess { get; set; }
-        public Error Errors { get; set; }
+        public Dictionary<string, List<string>> Errors { get; set; }
     }
     public class Response<T>
     {
-        public string StatusCode { get; set; }
+        public int StatusCode { get; set; }
         public string Message { get; set; }
         public bool IsSuccess { get; set; }
-        public Error Errors { get; set; }
-        public T Content { get; set; }
-    }
-
-    public class Error
-    {
-        public string Type { get; set; }
-        public string Title { get; set; }
-        public int Status { get; set; }
-        public string TraceId { get; set; }
         public Dictionary<string, List<string>> Errors { get; set; }
+        public T Content { get; set; }
     }
 }
